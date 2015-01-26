15eb22455d6476fd87510e903962d284e51a2d75
Roberto Morais <robertm@genebygene.com>
2015-01-26 17:40:28 -0600

    corrected production website name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@ set :rails_env, "production"
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'elasticgeeks.morais.us', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'elasticgeeks.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
