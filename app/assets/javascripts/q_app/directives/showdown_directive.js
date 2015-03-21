53c2d82413c533c1d45c2938e99e4cd5d54e6d23
Roberto Morais <robertm@genebygene.com>
2015-03-21 15:57:49 -0500

    Added Description to Categories and Items

--- app/assets/javascripts/q_app/directives/showdown_directive.js
+++ app/assets/javascripts/q_app/directives/showdown_directive.js
@@ -2,15 +2,37 @@ var app = angular.module('ngQApp.directives');
 
 app.directive('qMarkdown', [function () {
     var converter = new Showdown.converter();
+
+    function link2(scope, element, attrs) {
+
+        function updateData(value) {
+            if (value) {
+                var htmlText = converter.makeHtml(value);
+                element.html(htmlText);
+            }
+        }
+
+        window.console.log(attrs);
+        scope.$watch(attrs.qMarkdown, function(value) {
+            window.console.log('aa');
+            updateData(value);
+        });
+
+        updateData(scope.data);
+    }
+
     return {
         restrict: 'E',
         scope: {
             data: '='
         },
-        link: function (scope, element, attrs) {
-            var htmlText = converter.makeHtml(scope.data);
-            //var htmlText = converter.makeHtml(element.text());
-            element.html(htmlText);
-        }
+        link: link2
+        //link: function (scope, element, attrs) {
+        //    if (scope.data) {
+        //        var htmlText = converter.makeHtml(scope.data);
+        //        //var htmlText = converter.makeHtml(element.text());
+        //        element.html(htmlText);
+        //    }
+        //}
     }
 }]);
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,7 @@ class Category < ActiveRecord::Base
 
   has_many :sub_categories, class_name: 'Category', foreign_key:'parent_id'
   has_many :items
+  belongs_to :parent, class_name: 'Category', foreign_key: 'parent_id'
 
   default_scope -> {order('name ASC')}
 end
